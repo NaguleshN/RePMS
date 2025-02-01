5dfa1e0c34c8b195b6d8d4c6074d7f5c998e81f1
NaguleshN <nagulesh.22cs@kct.ac.in>
2025-02-01 17:41:42 +0530

    Update v2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,21 +3,39 @@ import HomeScreen from './pages/HomeScreen.jsx';
 import Apply from './pages/Apply.jsx';
 import Login from './pages/Login.jsx';
 import ListProjects from './pages/ListProjects.jsx';
+
 import CheckAuthentication from './pages/CheckAuthentication.jsx';
+
+import Home from './components/Home.jsx';
+import Themes from './components/Themes.jsx';
+import Contact from './components/Contact.jsx';
+import About from './components/About.jsx';
+import Navbar from './components/Navbar.jsx';
+import './styles/App.css';
 
 function App() {
 
   return (
+    
     <Router>
+      <Navbar />
+      <div className="main-content">
+
       <Routes>
-        <Route path="/" element={<HomeScreen />} />
+        {/* <Route path="/" element={<HomeScreen />} /> */}
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/theme" element={<Themes />} />
+        {/* <Route path="/add-project" element={<AddProject />} /> */}
+        
+        <Route path="/contact" element={<Contact />} />
         <Route path="/apply" element={<Apply />} />
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/allprojects" element={<ListProjects />} /> */}
         <Route element={<CheckAuthentication />}>
             <Route path="/allprojects" element={<ListProjects />} />
         </Route>
       </Routes>
+      </div>
     </Router>
   )
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import '../styles/Navbar.css'
+import imag from "../assets/re-img.png";
+
+function Navbar() {
+    return (
+        <header id="header" className="header d-flex align-items-center fixed-top">
+            <div className="container-fluid container-xl position-relative d-flex align-items-center justify-content-between">
+                <a href="/" className="logo d-flex align-items-center">
+                    <img src={imag} alt="Logo" className="logo-image" />
+                    <h1 className="sitename">Ré</h1>
+                </a>
+
+                <nav id="navmenu" className="navmenu">
+                <ul>
+                        <li><NavLink to="/" className={({ isActive }) => (isActive ? 'active' : '')}>Home</NavLink></li>
+                        <li><NavLink to="/about" className={({ isActive }) => (isActive ? 'active' : '')}>About Us</NavLink></li>
+                        <li><NavLink to="/contact" className={({ isActive }) => (isActive ? 'active' : '')}>Contact Us</NavLink></li>
+                        <li><NavLink to="/apply" className={({ isActive }) => (isActive ? 'active' : '')}>Apply Now</NavLink></li>
+                        <li><NavLink to="/login" className={({ isActive }) => (isActive ? 'active' : '')}>Login</NavLink></li>
+                        <li><NavLink to="/allprojects" className={({ isActive }) => (isActive ? 'active' : '')}>View projects</NavLink></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    );
+}
+
+export default Navbar;

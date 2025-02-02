4e582cc833b3fcab415bc45e060f14c8e74da14b
NaguleshN <nagulesh.22cs@kct.ac.in>
2025-02-02 17:51:19 +0530

    Update v3.2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,11 +9,5 @@ export default defineConfig({
   ],
   server: {
     port: 3000, 
-    proxy: {
-      '/api': {
-        target: import.meta.env.VITE_BASE_URL,
-        changeOrigin: true,
-      },
-    },
   },
 });

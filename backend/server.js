2c5e915a9941c16499e751615e112e1aa6294fbc
NaguleshN <nagulesh.22cs@kct.ac.in>
2025-02-02 18:04:52 +0530

    Update v3.2

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:3000","http://localhost:3001","https://repms.onrender.com"],
+    origin: ["http://localhost:3000","http://localhost:3001","https://repms-1.onrender.com"],
     credentials:true
 }
 ))

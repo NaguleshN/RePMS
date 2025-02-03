9d00de430c3eabe1d39154078abdabe89ff34d59
NaguleshN <nagulesh.22cs@kct.ac.in>
2025-02-03 14:51:52 +0530

    Update v3.2

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:3000","http://localhost:3001","https://repms-1.onrender.com",'https://repms-1.onrender.com'],
+    origin: ["http://localhost:3000","http://localhost:3001","https://repms-1.onrender.com",'https://repms-1.onrender.com',"https://jocular-chimera-c2f6fa.netlify.app"],
     credentials:true,
     methods: 'GET,POST,PUT,DELETE',
     allowedHeaders: 'Content-Type,Authorization',

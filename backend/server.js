dbd1b7b8dd63a82f9045112678a24e60fb49324b
NaguleshN <nagulesh.22cs@kct.ac.in>
2025-02-03 15:17:30 +0530

    Update v3.2

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ app.use(cookieParser())
 app.use(cors({
     origin: ["http://localhost:3000","http://localhost:3001","https://repms-1.onrender.com",'https://repms-1.onrender.com',"https://startling-faloodeh-0fe835.netlify.app/"],
     credentials:true,
-    methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
 }
 ))
 
